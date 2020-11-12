15b0f28229926f90e6f510262170d60684c6cef3
J K Nanda <jyotikaustuvnanda@MacBook-Pro.local>
2020-11-12 12:26:24 -0800

    chore: dev work

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,5 +1,5 @@
 import { ApolloServer, gql } from "apollo-server-micro";
-console.log("api/index.js");
+
 /*
   A schema is a collection of type definitions (hence "typeDefs")
   that together define the "shape" of queries that are executed against
